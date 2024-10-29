84835a3a3b9fc6ac29160ae5f7cd2a2e1241692d
Patrick Pircher <patrick.pircher@ibm.com>
2024-10-29 11:59:09 +0100

    stole from ember-primitives

---
+++ docs-app/ember-cli-build.js
@@ -0,0 +1,84 @@
+'use strict';
+
+const path = require('path');
+const fs = require('fs');
+const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = async function (defaults) {
+  const { readPackageUpSync } = await import('read-package-up');
+
+  const app = new EmberApp(defaults, {
+    'ember-cli-babel': {
+      enableTypeScriptTransform: true,
+      disableDecoratorTransforms: true,
+    },
+    babel: {
+      plugins: [
+        // add the new transform.
+        require.resolve('decorator-transforms'),
+      ],
+    },
+  });
+
+  const { kolay } = await import('kolay/webpack');
+
+  const { Webpack } = require('@embroider/webpack');
+
+  return require('@embroider/compat').compatBuild(app, Webpack, {
+    extraPublicTrees: [],
+    staticAddonTrees: true,
+    staticAddonTestSupportTrees: true,
+    staticHelpers: true,
+    staticModifiers: true,
+    staticComponents: true,
+    // ember-inspector does not work with this flag
+    // staticEmberSource: true,
+    packagerOptions: {
+      webpackConfig: {
+        devtool: 'source-map',
+        resolve: {
+          alias: {
+            path: 'path-browserify',
+          },
+          fallback: {
+            path: require.resolve('path-browserify'),
+            assert: require.resolve('assert'),
+            fs: false,
+          },
+        },
+        node: {
+          global: false,
+          __filename: true,
+          __dirname: true,
+        },
+        plugins: [
+          kolay({
+            src: 'public/docs',
+            packages: ['ember-native'],
+          }),
+        ],
+        module: {
+          rules: [
+            {
+              test: /\.(woff|woff2|eot|ttf|otf)$/,
+              use: ['file-loader'],
+            },
+            {
+              test: /.css$/i,
+              use: [
+                {
+                  loader: 'postcss-loader',
+                  options: {
+                    postcssOptions: {
+                      config: 'config/postcss.config.js',
+                    },
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      },
+    },
+  });
+};
