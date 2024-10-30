46f5a432eb942ac100bc589e6ba4b6c74d85cb64
patrickpircher <pa.trickjlp@gmail.com>
2024-10-30 18:05:44 +0100

    stolen from ember-primitives

---
+++ docs-app/app/app.ts
@@ -0,0 +1,33 @@
+import 'decorator-transforms/globals';
+import './css/styles.css';
+import 'inter-ui/inter-variable-latin.css';
+import '@fontsource/lexend';
+
+import Application from '@ember/application';
+
+import config from 'docs-app/config/environment';
+import loadInitializers from 'ember-load-initializers';
+import { sync } from 'ember-primitives/color-scheme';
+import Resolver from 'ember-resolver';
+
+import { install } from './icons';
+
+sync();
+install();
+
+// @babel/traverse (from babel-plugin-ember-template-imports)
+// accesses process.....
+// maybe one day we can have a browser-only version?
+// But they aren't used.... so.. that's fun.
+Object.assign(window, {
+  process: { env: {} },
+  Buffer: {},
+});
+
+export default class App extends Application {
+  modulePrefix = config.modulePrefix;
+  podModulePrefix = config.podModulePrefix;
+  Resolver = Resolver;
+}
+
+loadInitializers(App, config.modulePrefix);
---
+++ docs-app/app/routes/application.ts
@@ -0,0 +1,90 @@
+import Route from '@ember/routing/route';
+import { service } from '@ember/service';
+
+import rehypeShikiFromHighlighter from '@shikijs/rehype/core';
+import { Callout } from 'docs-app/components/callout';
+import { getHighlighterCore } from 'shiki/core';
+import getWasm from 'shiki/wasm';
+
+import { APIDocs, ComponentSignature, ModifierSignature } from './api-docs';
+
+import type { SetupService } from 'ember-primitives';
+import type { DocsService } from 'kolay';
+
+export default class Application extends Route {
+  @service('kolay/docs') declare docs: DocsService;
+  @service('ember-primitives/setup') declare primitives: SetupService;
+
+  beforeModel() {
+    this.primitives.setup();
+  }
+
+  async model() {
+    const highlighter = await getHighlighterCore({
+      themes: [import('shiki/themes/github-dark.mjs'), import('shiki/themes/github-light.mjs')],
+      langs: [
+        import('shiki/langs/javascript.mjs'),
+        import('shiki/langs/typescript.mjs'),
+        import('shiki/langs/bash.mjs'),
+        import('shiki/langs/css.mjs'),
+        import('shiki/langs/diff.mjs'),
+        import('shiki/langs/html.mjs'),
+        import('shiki/langs/glimmer-js.mjs'),
+        import('shiki/langs/glimmer-ts.mjs'),
+        import('shiki/langs/handlebars.mjs'),
+        import('shiki/langs/jsonc.mjs'),
+        import('shiki/langs/markdown.mjs'),
+      ],
+      loadWasm: getWasm,
+    });
+
+    await this.docs.setup({
+      apiDocs: import('kolay/api-docs:virtual'),
+      manifest: import('kolay/manifest:virtual'),
+      topLevelScope: {
+        Callout,
+        APIDocs,
+        ComponentSignature,
+        ModifierSignature,
+      },
+      resolve: {
+        // ember-primitives
+        'ember-primitives': import('ember-primitives'),
+        'ember-primitives/floating-ui': import('ember-primitives/floating-ui'),
+        'ember-primitives/color-scheme': import('ember-primitives/color-scheme'),
+        'ember-primitives/components/form': import('ember-primitives/components/form'),
+
+        // community libraries
+        'ember-resources': import('ember-resources'),
+        'ember-headless-form': import('ember-headless-form'),
+        'reactiveweb/remote-data': import('reactiveweb/remote-data'),
+        // @ts-expect-error - no types provided
+        'ember-focus-trap/modifiers/focus-trap': import('ember-focus-trap/modifiers/focus-trap'),
+        // @ts-expect-error - no types provided
+        'ember-focus-trap': import('ember-focus-trap'),
+
+        // utility
+        'lorem-ipsum': import('lorem-ipsum'),
+        'form-data-utils': import('form-data-utils'),
+        kolay: import('kolay'),
+      },
+      rehypePlugins: [
+        [
+          // @ts-expect-error - shiki may have the wrong type, since the other plugins are fine
+          rehypeShikiFromHighlighter,
+          highlighter,
+          {
+            // Theme chosen by CSS variables in app/css/site/shiki.css
+            defaultColor: false,
+            themes: {
+              light: 'github-light',
+              dark: 'github-dark',
+            },
+          },
+        ],
+      ],
+    });
+
+    return { manifest: this.docs.manifest };
+  }
+}
