5df59a7fab9c1f634a533cdc672d48201004b994
patrickpircher <pa.trickjlp@gmail.com>
2024-11-11 15:48:22 +0100

    show errors during runloop

--- ember-native/src/setup.ts
+++ ember-native/src/setup.ts
@@ -2,6 +2,7 @@ import * as loader from 'loader.js';
 import { registerElements } from './dom/setup-registry.ts';
 import { SimpleDynamicAttribute } from '@glimmer/runtime';
 import ElementNode from './dom/nodes/ElementNode.ts';
+import { _backburner } from '@ember/runloop';
 
 export function setup() {
   globalThis.requireModule = loader.require;
@@ -11,6 +12,16 @@ export function setup() {
   globalThis.Element = ElementNode as any;
   globalThis.Node = ElementNode as any;
   globalThis.HTMLElement = ElementNode as any;
+
+  function handleBackburnerErrors() {
+    const next = _backburner['_platform'].next;
+    _backburner['_platform'].next = function (...args: any) {
+      const p = next.call(this, ...args);
+      p.catch(console.error);
+      return p;
+    };
+  }
+  handleBackburnerErrors();
 
   SimpleDynamicAttribute.prototype.set = function (dom, value, _env) {
     const { name, namespace } = this.attribute;
