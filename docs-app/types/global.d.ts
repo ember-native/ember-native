04d69ae6d42b3119211f78c97f11620e15c773d7
patrickpircher <pa.trickjlp@gmail.com>
2024-12-02 17:33:45 +0100

    fix

--- docs-app/types/global.d.ts
+++ docs-app/types/global.d.ts
@@ -1,5 +1,7 @@
 declare namespace globalThis {
   var define: (p: string, cb: () => any) => void;
+  var require: any;
+  var requireModule: any;
 }
 
 declare module '@embroider/virtual/compat-modules';
