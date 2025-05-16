a681c11897cabad9455a9f1deb6cc3e7a314bdc4
patricklx <patricklx@users.noreply.github.com>
2025-05-16 20:01:53 +0000

    deploy: 5a7acd52fa20f11d8722bcca3dbfec9c2ea3403b

---
+++ versions/main/assets/form-BC7Tn4Ur.js
@@ -0,0 +1,29 @@
+import { s as setComponentTemplate, t as templateOnly, a as templateFactory, o as on, f as fn } from './main-DC_2B1Zk.js';
+import { dataFrom } from './index-dGgzbTWI.js';
+
+const dataFromEvent = dataFrom;
+const handleInput = (onChange, event, eventType = "input") => {
+  const data = dataFrom(event);
+  onChange(data, eventType, event);
+};
+const handleSubmit = (onChange, event) => {
+  event.preventDefault();
+  handleInput(onChange, event, "submit");
+};
+const Form = setComponentTemplate(templateFactory(
+/*
+  
+  <form {{on "input" (fn handleInput @onChange)}} {{on "submit" (fn handleSubmit @onChange)}} ...attributes>
+    {{yield}}
+  </form>
+
+*/
+{
+  "id": "j1vDqtkF",
+  "block": "[[[1,\"\\n  \"],[11,\"form\"],[17,1],[4,[32,0],[\"input\",[28,[32,1],[[32,2],[30,2]],null]],null],[4,[32,0],[\"submit\",[28,[32,1],[[32,3],[30,2]],null]],null],[12],[1,\"\\n    \"],[18,3,null],[1,\"\\n  \"],[13],[1,\"\\n\"]],[\"&attrs\",\"@onChange\",\"&default\"],false,[\"yield\"]]",
+  "moduleName": "/home/runner/work/ember-native/ember-native/node_modules/.pnpm/ember-primitives@0.32.0_@babel+core@7.27.1_@ember+test-helpers@5.2.2_@babel+core@7.27.1_@glin_6tfyhtclqroved25hlgpx5igfy/node_modules/ember-primitives/dist/components/form.js",
+  "scope": () => [on, fn, handleInput, handleSubmit],
+  "isStrictMode": true
+}), templateOnly(undefined, "form:Form"));
+
+export { Form, dataFromEvent, Form as default };
---
+++ versions/main/assets/index-CGrr6ZoV.js
@@ -0,0 +1,27 @@
+import { S as Service, l as assert, m as Selected, D as DocsService$1, p as Compiler } from './main-DC_2B1Zk.js';
+export { A as APIDocs, q as CommentQuery, x as Compiled, u as ComponentSignature, H as HelperSignature, M as ModifierSignature, w as addRoutes, y as getIndexPage, z as isCollection, B as isIndex } from './main-DC_2B1Zk.js';
+
+class DocsService extends Service {
+  _packages = [];
+  loadApiDocs = {};
+  get packages() {
+    (!(this._packages) && assert(`packages was never set. Did you forget to import 'kolay/api-docs:virtual' and set it to 'apiDocs' when calling docs.setup()?`, this._packages));
+    return this._packages;
+  }
+  load = name => {
+    (!(this.loadApiDocs) && assert(`loadApiDocs was never set, did you forget to pass it do docs.setup?`, this.loadApiDocs));
+    const loader = this.loadApiDocs[name];
+    return loader();
+  };
+}
+
+function registry(prefix) {
+  return {
+    [`${prefix}/services/kolay/api-docs`]: DocsService,
+    [`${prefix}/services/kolay/compiler`]: Compiler,
+    [`${prefix}/services/kolay/docs`]: DocsService$1,
+    [`${prefix}/services/kolay/selected`]: Selected
+  };
+}
+
+export { registry };
