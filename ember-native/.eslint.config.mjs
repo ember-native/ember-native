6d977e5505618b2b3a8b3b8a3f45c74d85e5619d
Patrick Pircher <patricklx@users.noreply.github.com>
2025-05-21 12:59:27 +0200

    fix lint config

---
+++ ember-native/.eslint.config.mjs
@@ -0,0 +1,30 @@
+'use strict';
+
+const { emberNativeGlobals } = require('./utils/eslint/ember-native');
+// eslint.config.js
+import { configs } from "@nullvoxpopuli/eslint-configs";
+
+const config = configs.ember(import.meta.dirname);
+
+export default [
+  ...config,
+  // your modifications here
+  // see: https://eslint.org/docs/user-guide/configuring/configuration-files#how-do-overrides-work
+  {
+    globals: {
+    'ensure-safe-component': true,
+    ...emberNativeGlobals,
+  },
+  {
+    files: ["**/*.ts", "**/*.gts"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+  {
+    files: ["*.{js,cjs}"],
+    rules: {
+      "n/no-unsupported-features": "off",
+    },
+  },
+];
