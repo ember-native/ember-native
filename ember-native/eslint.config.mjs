0e272c08d600d8ebcc3ac7d0cc101205d048e3f7
Patrick Pircher <patricklx@users.noreply.github.com>
2025-05-27 16:44:35 +0200

    fix

--- ember-native/eslint.config.mjs
+++ ember-native/eslint.config.mjs
@@ -24,7 +24,7 @@ import n from 'eslint-plugin-n';
 
 import babelParser from '@babel/eslint-parser';
 
-import emberNativeGlobals from './utils/eslint/ember-native';
+import emberNativeGlobals from './utils/eslint/ember-native.js';
 
 const parserOptions = {
     esm: {
