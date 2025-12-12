41ad8a9030c120d99333f6cc0f3e0a0c7ca962db
patrick <pa.trickjlp@gmail.com>
2025-12-12 13:47:51 +0100

    fix

--- demo-app/nativescript.config.ts
+++ demo-app/nativescript.config.ts
@@ -1,4 +1,23 @@
+const Module = require('node:module');
+const fs = require('node:fs');
+const path = require('node:path');
+const originalLoad = Module._load;
+Module._load = function (request, parent, isMain) {
+  if (request === '@rollup/rollup-win32-x64-msv' || parent?.id?.includes('rollup')) {
+    console.log('load', request, parent.id);
+  }
+
+  const parentFile = parent?.filename ? fs.realpathSync(parent?.filename) : undefined;
+  if (parentFile) {
+    parent.filename = parentFile;
+    parent.id = parentFile;
+    parent.path = path.dirname(parentFile);
+  }
+  return originalLoad.call(this, request, parent, isMain);
+}
+
 import { NativeScriptConfig } from '@nativescript/core';
+
 
 export default {
   id: 'org.nativescript.embernativedemo',
--- demo-app/vite.config.ts
+++ demo-app/vite.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite';
 import { extensions, classicEmberSupport, ember } from '@embroider/vite';
 import { babel } from '@rollup/plugin-babel';
 import { typescriptConfig } from '@nativescript/vite';
+import Module from 'module';
+
+const req = Module.prototype.require;
+Module.prototype.require = function (...args) {
+  console.log('require', ...args);
+  return req.call(this, ...args);
+}
 
 export default defineConfig(({ mode }) => {
   const config = typescriptConfig({ mode });
