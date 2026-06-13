a4a83f5e4efd870dc8aa971fb89b407c2e97d4bd
Patrick Pircher <patricklx@users.noreply.github.com>
2026-06-13 15:17:13 +0200

    Export NativeElementNode from index.ts (#362)

--- ember-native/src/index.ts
+++ ember-native/src/index.ts
@@ -2,3 +2,4 @@ export { setup } from './setup.ts';
 export { setupInspectorSupport } from './setup-inspector-support.ts';
 export * from './components/index.gts';
 export * from './modifiers/index.ts';
+export { default as NativeElementNode } from './dom/native/NativeElementNode.ts';
